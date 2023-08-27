94e6bf2d14736f450017e9d7b9a6964032234084
Yanis Lukes <Pendrokar@gmail.com>
2023-08-28 01:11:34 +0300

    Run xVAsynth alongside Mantella; resolves #9 (#11)
    * config: try/catch missing parameter
    
    * tts: run xvasynth, if not already running
    
    * tts: use_cleanup & use_sr xVASnyth options added to config.ini
    
    * tts: properly waits for xVASynth launch now
    
    * README.md: xVASynth now autolaunched

--- src/config_loader.py
+++ src/config_loader.py
@@ -1,35 +1,45 @@
 import configparser
+import logging
 
 class ConfigLoader:
     def __init__(self, file_name='config.ini'):
         config = configparser.ConfigParser()
         config.read(file_name)
 
-        self.language = config['Language']['language']
-        self.end_conversation_keyword = config['Language']['end_conversation_keyword']
-        self.goodbye_npc_response = config['Language']['goodbye_npc_response']
-        self.collecting_thoughts_npc_response = config['Language']['collecting_thoughts_npc_response']
+        try:
+            self.language = config['Language']['language']
+            self.end_conversation_keyword = config['Language']['end_conversation_keyword']
+            self.goodbye_npc_response = config['Language']['goodbye_npc_response']
+            self.collecting_thoughts_npc_response = config['Language']['collecting_thoughts_npc_response']
 
-        self.game_path = config['Paths']['skyrim_folder']
-        self.xvasynth_path = config['Paths']['xvasynth_folder']
-        self.mod_path = config['Paths']['mod_folder']+'\Sound\Voice\Mantella.esp'
+            self.game_path = config['Paths']['skyrim_folder']
+            self.xvasynth_path = config['Paths']['xvasynth_folder']
+            self.mod_path = config['Paths']['mod_folder']+'\Sound\Voice\Mantella.esp'
 
-        self.whisper_model = config['Microphone']['model_size']
-        self.whisper_process_device = config['Microphone']['process_device']
-        self.audio_threshold = config['Microphone']['audio_threshold']
-        self.pause_threshold = float(config['Microphone']['pause_threshold'])
-        self.listen_timeout = int(config['Microphone']['listen_timeout'])
+            self.whisper_model = config['Microphone']['model_size']
+            self.whisper_process_device = config['Microphone']['process_device']
+            self.audio_threshold = config['Microphone']['audio_threshold']
+            self.pause_threshold = float(config['Microphone']['pause_threshold'])
+            self.listen_timeout = int(config['Microphone']['listen_timeout'])
 
-        self.max_response_sentences = int(config['LanguageModel']['max_response_sentences'])
-        self.llm = config['LanguageModel']['model']
+            self.max_response_sentences = int(config['LanguageModel']['max_response_sentences'])
+            self.llm = config['LanguageModel']['model']
 
-        self.subtitles_enabled = config['HUD']['subtitles']
+            self.xvasynth_process_device = config['Speech']['tts_process_device']
+            self.use_cleanup = int(config['Speech']['use_cleanup'])
+            self.use_sr = int(config['Speech']['use_sr'])
 
-        self.debug_mode = config['Debugging']['debugging']
-        self.play_audio_from_script = config['Debugging']['play_audio_from_script']
-        self.debug_character_name = config['Debugging']['debugging_npc']
-        self.debug_use_mic = config['Debugging']['use_mic']
-        self.default_player_response = config['Debugging']['default_player_response']
-        self.debug_exit_on_first_exchange = config['Debugging']['exit_on_first_exchange']
+            self.subtitles_enabled = config['HUD']['subtitles']
 
-        self.prompt = config['Prompt']['prompt']
+            self.debug_mode = config['Debugging']['debugging']
+            self.play_audio_from_script = config['Debugging']['play_audio_from_script']
+            self.debug_character_name = config['Debugging']['debugging_npc']
+            self.debug_use_mic = config['Debugging']['use_mic']
+            self.default_player_response = config['Debugging']['default_player_response']
+            self.debug_exit_on_first_exchange = config['Debugging']['exit_on_first_exchange']
+
+            self.prompt = config['Prompt']['prompt']
+            pass
+        except Exception as e:
+            logging.error('Parameter missing/invalid in config.ini file!')
+            raise e
