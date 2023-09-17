e733d51a852eae0be5d94ef76d58121e96a244a4
art-from-the-machine <128241174+art-from-the-machine@users.noreply.github.com>
2023-09-17 15:55:42 +0100

    Changed modded NPC message in case it is a vanilla NPC Mantella cannot find

--- src/output_manager.py
+++ src/output_manager.py
@@ -53,7 +53,7 @@ class ChatManager:
             os.mkdir(f"{self.mod_folder}/{in_game_voice_folder}/")
 
             self.game_state_manager.write_game_info('_mantella_error_check', 'True')
-            logging.warn("Modded NPC detected. This NPC will work correctly once you restart Skyrim. To learn how to add memory, a background, and a voice model of your choosing to this NPC, see here: https://github.com/art-from-the-machine/Mantella#adding-modded-npcs")
+            logging.warn("Unknown NPC detected. This NPC will be able to speak once you restart Skyrim. To learn how to add memory, a background, and a voice model of your choosing to this NPC, see here: https://github.com/art-from-the-machine/Mantella#adding-modded-npcs")
             input('\nPress any key to exit...')
             sys.exit(0)
 
