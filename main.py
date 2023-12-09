d5362763832a9031108b2abde69e044fb8e08ff3
Mahe <markus.heikkila@iki.fi>
2023-12-09 19:30:37 +0200

    Fix debug mode, it now works for most characters in CSV and sets up the game state from there

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import traceback
 import src.tts as tts
 import src.stt as stt
 import src.chat_response as chat_response
@@ -208,5 +209,5 @@ except Exception as e:
     except:
         None
 
-    logging.error(f"Error: {e}")
+    logging.error("".join(traceback.format_exception(e)))
     input("Press Enter to exit.")
