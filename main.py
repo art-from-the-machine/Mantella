a9995e3ed7d7080f307c6c016089f0f451e7bb6d
Art from the Machine <artfromthemachine@outlook.com>
2025-02-07 14:22:11 +0100

    Increase version to 0_13 preview 3

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
             logging_file='logging.log', 
             language_file='data/language_support.csv')
 
-        mantella_version = '0.13 Preview 2'
+        mantella_version = '0.13 Preview 3'
         logging.log(24, f'\nMantella v{mantella_version}')
 
         mantella_http_server = http_server()
