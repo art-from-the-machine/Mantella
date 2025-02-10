eaeabb5945f4d91cc335d0abcd9e6911aeec722f
art-from-the-machine <128241174+art-from-the-machine@users.noreply.github.com>
2025-02-10 14:25:41 +0100

    Delete unused STT route (#511)

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from src.http.http_server import http_server
 import traceback
 from src.http.routes.routeable import routeable
 from src.http.routes.mantella_route import mantella_route
-from src.http.routes.stt_route import stt_route
 import logging
 import src.setup as setup
 from src.ui.start_ui import StartUI
@@ -21,9 +20,8 @@ def main():
 
         should_debug_http = config.show_http_debug_messages
         conversation = mantella_route(config, 'STT_SECRET_KEY.txt', 'IMAGE_SECRET_KEY.txt', 'GPT_SECRET_KEY.txt', language_info, should_debug_http)
-        stt = stt_route(config, 'STT_SECRET_KEY.txt', 'GPT_SECRET_KEY.txt', should_debug_http)
         ui = StartUI(config)
-        routes: list[routeable] = [conversation, stt, ui]
+        routes: list[routeable] = [conversation, ui]
         
         mantella_http_server.start(int(config.port), routes, config.play_startup_sound, should_debug_http)
 
