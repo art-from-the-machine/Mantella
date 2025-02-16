8123dc4ad5a0db50b333a9808328b8892b3d6299
art-from-the-machine <128241174+art-from-the-machine@users.noreply.github.com>
2025-02-16 16:47:03 +0100

    Add option to save files to original voice folder (#522)
    * Initial commit
    
    * Create voice folders if they do not exist

--- src/games/fallout4.py
+++ src/games/fallout4.py
@@ -189,20 +189,29 @@ class fallout4(gameable):
     @utils.time_it
     def prepare_sentence_for_game(self, queue_output: sentence, context_of_conversation: context, config: ConfigLoader, topicID: int, isFirstVoiceLine: bool):
         audio_file = queue_output.voice_file
+        if not os.path.exists(audio_file):
+            return
+        
+        mod_folder = config.mod_path
         fuz_file = audio_file.replace(".wav",".fuz")
         speaker = queue_output.speaker
 
         lip_name = "00001ED2_1"
-        voice_name = "MantellaVoice00"
 
-        if not os.path.exists(audio_file):
-            return
-        mod_folder = config.mod_path
+        if config.save_audio_data_to_character_folder:
+            voice_folder_path = os.path.join(mod_folder,queue_output.speaker.in_game_voice_model)
+            if not os.path.exists(voice_folder_path):
+                logging.warning(f"{voice_folder_path} has been created for the first time. Please restart Fallout 4 to interact with this NPC.")
+                logging.info("Creating voice folders...")
+                self._create_all_voice_folders(mod_folder, "fallout4_voice_folder")
+        else:
+            voice_folder_path = os.path.join(mod_folder, "MantellaVoice00")
+        os.makedirs(voice_folder_path, exist_ok=True)
         
         # subtitle = queue_output.sentence
         # Copy FaceFX generated FUZ file
         try:
-            fuz_filepath = os.path.normpath(f"{mod_folder}/{voice_name}/{lip_name}.fuz")
+            fuz_filepath = os.path.normpath(f"{voice_folder_path}/{lip_name}.fuz")
             shutil.copyfile(fuz_file, fuz_filepath)
         except Exception as e:
             # only warn on failure
