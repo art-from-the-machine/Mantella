0364b14c4f2b0ea0a94799eeb9407b1701c19d16
Art from the Machine <artfromthemachine@outlook.com>
2025-04-29 19:04:20 +0200

    Fix Piper class capitalisation

--- tests/tts/test_piper.py
+++ tests/tts/test_piper.py
@@ -4,7 +4,7 @@ from src.tts.synthesization_options import SynthesizationOptions
 
 def test_piper_model_retrieval(piper: Piper):
     '''Test that at least the base Skyrim models are available'''
-    assert len(piper._piper__available_models) >= 68
+    assert len(piper._Piper__available_models) >= 68
 
 
 def test_select_voice_type(piper: Piper):
@@ -52,17 +52,18 @@ def test_select_voice_type(piper: Piper):
 def test_voice_models(piper: Piper):
     '''Test that voice models can be loaded and voicelines synthesized'''
     count = 0
-    num_models_to_check = 10
+    num_models_to_check = 10 # Limit the number of models to load or we'll be here all day
     synth_options = SynthesizationOptions(0, 0)
 
-    for voice_model in piper._piper__available_models:
+    for voice_model in piper._Piper__available_models:
         piper.change_voice(voice_model)
         piper._check_voice_changed()
-        assert piper._piper__selected_voice == voice_model
+        assert piper._Piper__selected_voice == voice_model
 
         voiceline_path = f'{piper._voiceline_folder}/out.wav'
         piper.tts_synthesize('Hello.', voiceline_path, synth_options)
         assert os.path.exists(voiceline_path)
+        # Optionally, play the synthesized voiceline:
         # winsound.PlaySound(voiceline_path, winsound.SND_FILENAME)
 
         # Voicelines are normally saved to individual files, but in this case just delete the existing file for the next run
