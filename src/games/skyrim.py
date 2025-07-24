ffc8860486347924411888e719267c5f1f474508
dreaminalife <yujigaoo@gmail.com>
2025-07-24 01:01:26 -0500

    create dynamic bio injection system

--- src/games/skyrim.py
+++ src/games/skyrim.py
@@ -12,6 +12,7 @@ from src.games.external_character_info import external_character_info
 from src.games.gameable import Gameable
 import src.utils as utils
 from src.config.definitions.tts_definitions import TTSEnum
+from src.bio_template_manager import BioTemplateManager
 
 
 class Skyrim(Gameable):
@@ -30,6 +31,9 @@ class Skyrim(Gameable):
         super().__init__(config, 'data/Skyrim/skyrim_characters.csv', "Skyrim")
         self.__tts_service: TTSEnum = config.tts_service
         self.__image_analysis_filepath = ""
+
+        # Initialize bio template manager with override support
+        self.__bio_template_manager = BioTemplateManager('data/Skyrim/bio_templates', config)
 
         try:
             weather_file = 'data/Skyrim/skyrim_weather.csv'
@@ -64,7 +68,12 @@ class Skyrim(Gameable):
         character_info, is_generic_npc = self.find_character_info(base_id, name, race, gender, ingame_voice_model)
         actor_voice_model_name = ingame_voice_model.split('<')[1].split(' ')[0]
 
-        return external_character_info(name, is_generic_npc, character_info["bio"], actor_voice_model_name, character_info['voice_model'], character_info['skyrim_voice_folder'], character_info['advanced_voice_model'], character_info.get('voice_accent', None), character_info.get('llm_service', ''), character_info.get('model', ''))
+        # Process bio with tags if they exist
+        base_bio = character_info["bio"]
+        tags = character_info.get("tags", "")
+        expanded_bio = self.__bio_template_manager.expand_bio_with_tags(base_bio, tags)
+
+        return external_character_info(name, is_generic_npc, expanded_bio, actor_voice_model_name, character_info['voice_model'], character_info['skyrim_voice_folder'], character_info['advanced_voice_model'], character_info.get('voice_accent', None), character_info.get('llm_service', ''), character_info.get('model', ''))
     
     @utils.time_it
     def find_best_voice_model(self, actor_race: str | None, actor_sex: int | None, ingame_voice_model: str, library_search:bool = True) -> str:
